8ca531a9f44f5b5564e6afa14aa48f01d5757ae4
Fernando Alves <fernando.alves@superteia.com.br>
2019-10-08 09:16:26 -0300

    setStatetopList

--- src/screens/products/ProductScreen.js
+++ src/screens/products/ProductScreen.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet,FlatList, Button } from 'react-native';
+import axios from 'axios'
 
 export default class ProductScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      buracos: []
+      // buracos: [
+      //   {"created_at": 'fernando'},
+      //   {"created_at": 'caio'},
+      // ]
     };
   }
 
+  _buracos = async () => {
+    await axios.get('https://codeforsuzanoapi.herokuapp.com/buraco')
+      .then(res => {
+        this.setState({
+          buracos: res.data
+        })
+        
+        console.log(this.state.buracos)
+        // this.props.navigation.navigate('HomeScreen');
+      })
+      .catch(function (error) {
+        // handle error
+        alert(error);
+      })
+  };
+
   render() {
     return (
-      <View>
-        <Text> ProductScreen </Text>
+      <View style={styles.container}>
+      <Button title='lista' onPress={this._buracos}/>
+        <FlatList
+          data={this.state.buracos}
+          renderItem={({item}) => <Text style={styles.item}>{item.created_at}</Text>}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
